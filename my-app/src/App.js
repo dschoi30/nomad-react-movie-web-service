1640c0ff830e91333a6b8666453edf7babb9da10
dschoi30 <ds0631@gmail.com>
2023-12-03 11:45:14 +0900

    Coin Tracker 구현

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+function LeftMenu({text}) {
+  return (
+    <span
+    style={{
+      width: "100px",
+      display: "inline-block",
+    }}
+    > { text }
+    </span>
+  )
+}
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [exchaningAmount, setAmount] = useState(0);
+  const [coinPrice, selectCoin] = useState(0);
+  const selectedCoin = (event) => selectCoin(event.target.value);
+  const inputAmount = (event) => setAmount(event.target.value);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index)  => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length} coins listed)`}</h1>
+      <LeftMenu text="Amounts($) : " />
+      <input type="number" value={exchaningAmount} onChange={inputAmount} />
+      <p></p>
+      <LeftMenu text="Coin : " />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={selectedCoin}>
+          <option>Select coin</option>
+          {coins.map((coin) => (
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {(exchaningAmount === 0 || coinPrice === 0) ? (
+        <p>Input the amounts($) you want to exchange</p>
+      ) : (
+        <p>Exchanged amounts : {(exchaningAmount / coinPrice).toFixed(2)}</p>
+      )}
     </div>
   );
 }
