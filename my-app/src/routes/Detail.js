866860c6a60c09bb078859fb6183089e343a8bb5
dschoi30 <ds0631@gmail.com>
2024-02-24 21:21:28 +0900

    Movie List, detail 기능구현

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function Detail() {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+    const [movieData, setMovieData] = useState(null);
+    const getMovie = async () => {
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json.data)
+        setMovieData(json.data);
+    };
+    useEffect(() => {
+        getMovie();
+    }, [id]);
+
+    return ( 
+        <div>
+            {movieData ? (
+                <h1>{movieData.movie.title_long}</h1>
+                ) : (
+                    <h1>Loading...</h1>
+                )
+            }        
+        </div>
+    );
 }
 
 export default Detail;
